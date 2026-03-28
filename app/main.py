bbe86d7cc1a98abcc7fb6e259fb16424a3d6243d
Suraj Kembale <surajkembale542@gmail.com>
2026-03-29 02:06:25 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -43,7 +43,7 @@ if settings.ENVIRONMENT == "production":
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=cors_origins,#{"*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
